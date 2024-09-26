dfe1e3777906d974421f30d4fc362268f64d2a6e
jared <placeholder@placeholder.com>
2024-09-25 19:06:46 -0700

    Defined numbers class, began numbers.cpp definition

---
+++ Module5/Homework5/Numbers.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "Numbers.h"
+#include <iostream>
+
+using namespace std;
+
+Numbers::Numbers(int number)
+{
+    this->number = number;
+}
+
+void Numbers::print() const
+{
+    
+}
--- Module5/Homework5/Numbers.h
+++ Module5/Homework5/Numbers.h
@@ -1,8 +1,34 @@
 #pragma once
+#include <string>
+
 
 class Numbers
 {
     private:
+        static const std::string
+            less_than_20[20],
+            tens[9],
+            hundred,
+            thousand;
         int number;
+    public:
+        Numbers(int); //constructor
+        void print() const; //print function (const because class should not be altered)
     
 };
+
+const std::string Numbers::less_than_20[] = {
+    "zero", "one", "two", "three", "four", "five", "six",
+    "seven", "eight", "nine", "ten", "eleven", "twelve",
+    "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+    "eighteen", "nineteen"
+};
+
+const std::string Numbers::tens[] = {
+    "ten", "twenty", "thirty", "fourty", "fifty",
+    "sixty", "seventy", "eighty", "ninety"
+};
+
+const std::string Numbers::hundred = "hundred";
+
+const std::string Numbers::thousand = "thousand";
