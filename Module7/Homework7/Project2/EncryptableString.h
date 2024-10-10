801eed8adaf2361ff0f2034264ab8ef1def811a5
MCCLAB4804WD10 <MCCLAB4804WD10@usa.miracosta.cc.ca.us>
2024-10-09 18:15:32 -0700

    Completed hmwk7 proj2

--- Module7/Homework7/Project2/EncryptableString.h
+++ Module7/Homework7/Project2/EncryptableString.h
@@ -5,48 +5,49 @@ using namespace std;
 
 class EncryptableString : public string
 {
-    EncryptableString(string obj) : string(obj) {} //string obj constructor
-    EncryptableString(char *cstr) : string(cstr) {} //cstring constructor
+    public:
+        EncryptableString(string obj) : string(obj) {} //string obj constructor
+        EncryptableString(char *cstr) : string(cstr) {} //cstring constructor
 
-    void encrypt()
-    {
-        //iterate through the string
-        for (int i = 0; i < this->size(); i++)
+        void encrypt()
         {
-            //checks if character is between 'a' and 'z' (z not inclusive)
-            if (this->at(i) < 122 && this->at(i) >= 97)
+            //iterate through the string
+            for (int i = 0; i < this->size(); i++)
             {
-                //increment character
-                this->at(i)++;
-            }
-            //checks if character is 'z'
-            else if (this->at(i) == 122)
-            {
-                //wraps around to 'a'
-                this->at(i) = 97;
-            }
-            //checks if character is between 'A' and 'Z' (Z not inclusive)
-            else if (this->at(i) < 90 && this->at(i) >= 65)
-            {
-                this->at(i)++;
-            }
-            //checks if character is 'Z'
-            else if (this->at(i) == 90)
-            {
-                //wraps around to 'A'
-                this->at(i) = 65;
-            }
-            //checks if character is between '0' and '9' (9 not inclusive)
-            else if (this->at(i) < 57 && this->at(i) >= 48)
-            {
-                this->at(i)++;
-            }
-            //checks if character is '9'
-            else if (this->at(i) == 57)
-            {
-                //wraps around to '0'
-                this->at(i) = 0;
+                //checks if character is between 'a' and 'z' (z not inclusive)
+                if (this->at(i) < 122 && this->at(i) >= 97)
+                {
+                    //increment character
+                    this->at(i)++;
+                }
+                //checks if character is 'z'
+                else if (this->at(i) == 122)
+                {
+                    //wraps around to 'a'
+                    this->at(i) = 97;
+                }
+                //checks if character is between 'A' and 'Z' (Z not inclusive)
+                else if (this->at(i) < 90 && this->at(i) >= 65)
+                {
+                    this->at(i)++;
+                }
+                //checks if character is 'Z'
+                else if (this->at(i) == 90)
+                {
+                    //wraps around to 'A'
+                    this->at(i) = 65;
+                }
+                //checks if character is between '0' and '9' (9 not inclusive)
+                else if (this->at(i) < 57 && this->at(i) >= 48)
+                {
+                    this->at(i)++;
+                }
+                //checks if character is '9'
+                else if (this->at(i) == 57)
+                {
+                    //wraps around to '0'
+                    this->at(i) = 0;
+                }
             }
         }
-    }
 };
--- Module7/Homework7/Project2/hmwk7_proj2.cpp
+++ Module7/Homework7/Project2/hmwk7_proj2.cpp
@@ -1,3 +1,25 @@
+/* hmwk7_proj2.cpp - demonstrates functionality of EncryptableString class
+ * Author: Jared Ziv
+ * Module: 7
+ * Project: 2
+ * Description:
+ *      Utilizes the EncryptableString class (inherits from string) by
+ *      creating various objects using different strings and running their
+ *      encrypt function twice, printing the result both times.
+ *  Algorithm:
+ *      1. The main file uses the string object constructor to create objects
+ *         of varying EncryptableStrings.
+ *      2. After printing the EncryptableString, encrypt() is called.
+ *          a. When encrypt() is called, iterate through the string using
+ *             a for loop until string size - 1.
+ *          b. At each iteration, check if the character at the index is between
+ *             ASCII values for 'a' and 'z', 'A' and 'Z', or '0' and '9' (second
+ *             values not inclusive)
+ *              - If true, iterate the character by 1 (char++)
+ *          c. At each iteration, also check if the character IS 'z', 'Z', or '9'
+ *              - If true, instead of iterating the character, loop the character
+ *                back to its respective starting value (i.e. 'a', 'A', or '0')        
+ */
 #include <iostream>
 #include <iomanip>
 #include "EncryptableString.h"
@@ -6,6 +28,44 @@ using namespace std;
 
 int main()
 {
-    //update to include test cases
+    string test_string = "Holy Moly!";
+    EncryptableString *holy = new EncryptableString(test_string);
+    cout << setw(20) << left << "Test string"<< ": " << *holy << endl;
+    holy->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *holy << endl;
+    holy->encrypt();
+    cout << setw(20) << left << "encrypt() twice"<< ": " << *holy << endl << endl;
+
+    test_string = "San Diego Zoo.";
+    EncryptableString *zoo = new EncryptableString(test_string);
+    cout << setw(20) << left << "Test string"<< ": " << *zoo << endl;
+    zoo->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *zoo << endl;
+    zoo->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *zoo << endl << endl;
+
+    test_string = "The cost is $25.89.";
+    EncryptableString *cost = new EncryptableString(test_string);
+    cout << setw(20) << left << "Test string"<< ": " << *cost << endl;
+    cost->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *cost << endl;
+    cost->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *cost << endl << endl;
+
+    test_string = "Go away – unless you have tacos.";
+    EncryptableString *tacos = new EncryptableString(test_string);
+    cout << setw(20) << left << "Test string"<< ": " << *tacos << endl;
+    tacos->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *tacos << endl;
+    tacos->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *tacos << endl << endl;
+
+    test_string = "No one can make you feel inferior without your consent.";
+    EncryptableString *consent = new EncryptableString(test_string);
+    cout << setw(20) << left << "Test string"<< ": " << *consent << endl;
+    consent->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *consent << endl;
+    consent->encrypt();
+    cout << setw(20) << left << "encrypt() once"<< ": " << *consent << endl << endl;
     return 0;
 }
