89f99ec2c39e3c7c670920103fcd25afd3a99ccf
jazi2018 <jaredaziv@gmail.com>
2024-10-16 21:37:38 -0700

    completed hmwk 8 proj 2

--- Module8/Homework8/Project2/hmwk8_proj2.cpp
+++ Module8/Homework8/Project2/hmwk8_proj2.cpp
@@ -8,6 +8,7 @@
  *   1.  <detailed algorithm>
  */
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -15,6 +16,39 @@ bool checkPass(string str);
 
 int main()
 {
+    /*
+     * I am confused about the order of operations for the main function
+     * In the problem statement, it says it should "return" a list of the
+     * errors and also return true or false. I assumed that meant print
+     * the list of errors. But after, it says that upon return from the
+     * function, it should say if it was accepted or rejected, and if
+     * it was rejected, a list of reasons why. I interpreted that as an
+     * ordered sequence, where it would print "Password rejected" and
+     * THEN the list. I could think of a few ways to implement it (like
+     * maybe using a class?) but the specifications were pretty clear about
+     * passing strings and returning true or false so I went the path of
+     * least resistance :) If there's any issues with my implementation
+     * just let me know and I will fix it.
+     */
+
+    bool valid_pass = false;
+    while (!valid_pass)
+    {
+        cout << "Enter a password:" << endl;
+        string password;
+        cin >> password; //cin is space delimited so there shouldnt be spaces anyways
+
+        valid_pass = checkPass(password);
+
+        if (valid_pass)
+        {
+            cout << "The password entered is valid." << endl;
+        }
+        else
+        {
+            cout << "The password entered is invalid." << endl;
+        }
+    }
 
     return 0;
 }
@@ -32,31 +66,36 @@ bool checkPass(string str)
 
     //check for upper, lower, digit and blank characters
     bool has_upper = false, has_lower = false, has_blanks = false, has_digit = false;
-    for (int i = 0; i < str.length(); i++)
+    for (char c : str)
     {
-        if (str[i] >= 65 && str[i] <= 90)
+        if (isupper(c))
         {
             has_upper = true;
         }
-        else if (str[i] >= 97 && str[i] <= 122)
+        else if (islower(c))
         {
             has_lower = true;
         }
-        else if (isdigit(str[0]))
+        else if (isdigit(c))
         {
             has_digit = true;
         }
-        else if (str[0] == ' ')
+        else if (isspace(c))
         {
             has_blanks = true;
         }
     }
 
     //report appropriate errors
-    if (!has_upper || !has_lower)
+    if (!has_upper)
     {
-        cout << "Password must contain at least one uppercase and lowercase letter." << endl;
+        cout << "Password must contain at least one uppercase letter." << endl;
         valid_pass = false;
+    }
+
+    if (!has_lower)
+    {
+        cout << "Password must contain at least one lowercase letter." << endl;
     }
 
     if(!has_digit)
